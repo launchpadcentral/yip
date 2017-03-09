c24e5ca2fd48fdc0836c5d8de96a15afa68afc84
Can Yucel <can.yucel@gmail.com>
2017-03-08 22:01:28 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	ErrKeyNotFound   = errors.New("key not found")
+	ErrInvalidObject = errors.New("invalid object")
+	ErrInvalidIndex  = errors.New("invalid index")
+	ErrInvalidKey    = errors.New("invalid key")
+
+	keyPairs   string
+	outputFile string
+	inputFile  string
+)
+
+func parseFlags() {
+	flag.StringVar(&keyPairs, "key-pairs", "", "key-pairs input needs to be in key=pair format. i.e. api.tag=123,srv.image=234")
+	flag.StringVar(&inputFile, "f", "", "input file name")
+	flag.StringVar(&outputFile, "o", "", "output file name")
+	flag.Parse()
+
+	if keyPairs == "" {
+		log.Fatalf("-key-pairs flag is not set")
+	}
+
+	if inputFile == "" {
+		log.Fatalf("-f is not set")
+	}
+}
+
+func main() {
+
+	updates := validateInput()
+	t := yamlInput()
+
+	var err error
+	for i := 0; i < len(updates); i += 2 {
+		var newValue interface{}
+		newValue = updates[i+1]
+		if intVal, err := strconv.Atoi(updates[i+1]); err == nil {
+			newValue = intVal
+		}
+		t, err = updateYaml(t, updates[i], newValue)
+		if err != nil {
+			log.Fatalf("Error with key %s: %s", updates[i], err)
+			return
+		}
+	}
+
+	output(t)
+}
+
+func validateInput() []string {
+	parseFlags()
+	updates := parseInput()
+
+	if len(updates)%2 != 0 {
+		log.Fatalf("-key-pairs input needs to be in key=pair format. i.e. api.tag=123,srv.image=234")
+	}
+
+	return updates
+}
+
+func yamlInput() interface{} {
+	data := mustReadYaml()
+
+	var t interface{}
+	if err := yaml.Unmarshal([]byte(data), &t); err != nil {
+		log.Fatalf("Could not unmarshal yaml file: %s", err)
+	}
+
+	return t
+}
+
+func output(t interface{}) {
+	d, err := yaml.Marshal(&t)
+	if err != nil {
+		log.Fatalf("Could not marshal yaml file: %s", err)
+	}
+
+	if outputFile == "" {
+		fmt.Printf("%+v\n", string(d))
+		return
+	}
+
+	writeYaml(d)
+}
+
+func mustReadYaml() []byte {
+	dat, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("Could not read input file: %s", err)
+	}
+
+	return dat
+}
+
+func writeYaml(output []byte) {
+	if err := ioutil.WriteFile(outputFile, output, 0644); err != nil {
+		log.Fatalf("Could not write the output to the file: %s", err)
+	}
+}
+
+func parseInput() []string {
+	pairs := strings.Split(keyPairs, ",")
+	input := []string{}
+	for i := range pairs {
+		input = append(input, strings.Split(strings.Trim(pairs[i], " "), "=")...)
+	}
+
+	return input
+}
+
+func updateYaml(input interface{}, key string, value interface{}) (interface{}, error) {
+	keys := strings.Split(key, ".")
+
+	currentKey := keys[0]
+
+	nextKey := strings.Join(keys[1:], ".")
+	switch input.(type) {
+	case map[interface{}]interface{}:
+		subMap := input.(map[interface{}]interface{})
+
+		val, err := updateYaml(subMap[currentKey], nextKey, value)
+		if err != nil {
+			return nil, err
+		}
+		subMap[currentKey] = val
+		return subMap, nil
+	case []interface{}:
+		subArray := input.([]interface{})
+		index, err := strconv.Atoi(currentKey)
+		if err != nil {
+			return nil, ErrInvalidIndex
+		}
+
+		if index >= len(subArray) {
+			return nil, ErrKeyNotFound
+		}
+
+		val, err := updateYaml(subArray[index], nextKey, value)
+		if err != nil {
+			return nil, err
+		}
+		subArray[index] = val
+
+		return subArray, nil
+	case string, int:
+		if len(keys) == 0 || keys[0] == "" {
+			return value, nil
+		}
+		return nil, ErrInvalidKey
+	}
+
+	return nil, ErrKeyNotFound
+}
---
+++ main_test.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+var singleKeyInput = `
+key: value
+`
+
+var multipleKeyInput = `
+audit-api:
+  api:
+    imageTag: latest
+`
+var listInput = `
+audit-api:
+  - api:
+      imageTag: latest
+  - api:
+      imageTag: something
+`
+var primitiveListInput = `audit-api:
+- api1
+- api2
+`
+
+func readYaml(t *testing.T, input string) (output map[interface{}]interface{}) {
+	err := yaml.Unmarshal([]byte(input), &output)
+	if err != nil {
+		t.Fatalf("expected nil, got %s", err)
+	}
+
+	return
+}
+
+func testWriteYaml(t *testing.T, input interface{}) string {
+	res, err := yaml.Marshal(input)
+	if err != nil {
+		t.Fatalf("expected nil, got %s", err)
+	}
+
+	return string(res)
+}
+
+func TestUpdateYamlSingleKeyNotFound(t *testing.T) {
+	yaml := readYaml(t, singleKeyInput)
+
+	_, err := updateYaml(yaml, "test", "123")
+	if err != ErrKeyNotFound {
+		t.Errorf("expected %s, got %s", ErrKeyNotFound, err)
+	}
+}
+
+func TestUpdateYamlSingleKeySuccess(t *testing.T) {
+	yaml := readYaml(t, singleKeyInput)
+
+	output, err := updateYaml(yaml, "key", 123)
+	if err != nil {
+		t.Fatalf("expected nil, got %s", err)
+	}
+
+	res := testWriteYaml(t, output)
+	expectedYaml := `key: 123
+`
+	if res != expectedYaml {
+		t.Fatalf("expected:\n%#v\n,got:\n%#v", expectedYaml, res)
+	}
+
+}
+
+func TestUpdateYamlMultipleKeyNotFound(t *testing.T) {
+	yaml := readYaml(t, multipleKeyInput)
+
+	_, err := updateYaml(yaml, "card-api.api.latest", "123")
+	if err != ErrKeyNotFound {
+		t.Errorf("expected %s, got %s", ErrKeyNotFound, err)
+	}
+}
+
+func TestUpdateYamlMultipleKeyInvalidObject(t *testing.T) {
+	var input = `
+audit-api:
+  api: latest
+`
+	yaml := readYaml(t, input)
+
+	_, err := updateYaml(yaml, "audit-api.api.imageTag", 123)
+	if err != ErrInvalidKey {
+		t.Errorf("expected %s, got %v", ErrInvalidObject, err)
+	}
+
+}
+
+func TestUpdateYamlMultipleKey(t *testing.T) {
+	yaml := readYaml(t, multipleKeyInput)
+
+	output, err := updateYaml(yaml, "audit-api.api.imageTag", 123)
+	if err != nil {
+		t.Errorf("expected nil, got %s", err)
+	}
+
+	res := testWriteYaml(t, output)
+	expectedYaml := `audit-api:
+  api:
+    imageTag: 123
+`
+	if res != expectedYaml {
+		t.Fatalf("expected:\n%#v\n,got:\n%#v", expectedYaml, res)
+	}
+}
+
+func TestUpdateYamlList(t *testing.T) {
+	yaml := readYaml(t, listInput)
+
+	output, err := updateYaml(yaml, "audit-api.1.api.imageTag", 123)
+	if err != nil {
+		t.Fatalf("expected nil, got %s", err)
+	}
+
+	res := testWriteYaml(t, output)
+	expectedYaml := `audit-api:
+- api:
+    imageTag: latest
+- api:
+    imageTag: 123
+`
+	if res != expectedYaml {
+		t.Fatalf("expected:\n%#v\n,got:\n%#v", expectedYaml, res)
+	}
+}
+
+func TestUpdateYamlListWithPrimitiveElements(t *testing.T) {
+	yaml := readYaml(t, primitiveListInput)
+
+	output, err := updateYaml(yaml, "audit-api.1", 123)
+	if err != nil {
+		t.Fatalf("expected nil, got %s", err)
+	}
+
+	res := testWriteYaml(t, output)
+	expectedYaml := `audit-api:
+- api1
+- 123
+`
+	if res != expectedYaml {
+		t.Fatalf("expected:\n%#v\n,got:\n%#v", expectedYaml, res)
+	}
+}
+
+func TestUpdateYamlListWithOOIndexPrimitiveElements(t *testing.T) {
+	yaml := readYaml(t, primitiveListInput)
+
+	_, err := updateYaml(yaml, "audit-api.3", 123)
+	if err != ErrKeyNotFound {
+		t.Fatalf("expected %s, got %s", ErrKeyNotFound, err)
+	}
+
+}
+
+func TestUpdateYamlListOutOfIndex(t *testing.T) {
+	yaml := readYaml(t, primitiveListInput)
+
+	_, err := updateYaml(yaml, "audit-api.3", 123)
+	if err != ErrKeyNotFound {
+		t.Fatalf("expected %s, got %s", ErrKeyNotFound, err)
+	}
+}
+
+func TestUpdateYamlListInvalidIndex(t *testing.T) {
+	yaml := readYaml(t, primitiveListInput)
+
+	_, err := updateYaml(yaml, "audit-api.key", 123)
+	if err != ErrInvalidIndex {
+		t.Fatalf("expected %s, got %s", ErrInvalidIndex, err)
+	}
+
+}
+
+func TestUpdateYamlListPrimitiveElementInvalidKey(t *testing.T) {
+	yaml := readYaml(t, primitiveListInput)
+
+	_, err := updateYaml(yaml, "audit-api.1.test", 123)
+	if err != ErrInvalidKey {
+		t.Fatalf("expected %s, got %s", ErrInvalidKey, err)
+	}
+
+}
+
+func TestParseInput(t *testing.T) {
+	keyPairs = "kubernetes.password=123,user-api.api.imageTag=1q2w3e"
+
+	output := parseInput()
+	expected := []string{"kubernetes.password", "123", "user-api.api.imageTag", "1q2w3e"}
+	if !reflect.DeepEqual(expected, output) {
+		t.Fatalf("expected %+v, got %+v", expected, output)
+	}
+}
+
+func TestParseInputWithSpaces(t *testing.T) {
+	keyPairs = "    kubernetes.password=123,    user-api.api.imageTag=1q2w3e   "
+
+	output := parseInput()
+	expected := []string{"kubernetes.password", "123", "user-api.api.imageTag", "1q2w3e"}
+	if !reflect.DeepEqual(expected, output) {
+		t.Fatalf("expected %+v, got %+v", expected, output)
+	}
+}
